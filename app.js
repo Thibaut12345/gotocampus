88cf4c109d9ea9ed736cc2a4891d5dd4cc4dc6bf
Thibaut Bruyneel <thibaut.bruyneel@gmail.com>
2026-03-12 15:41:52 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const path = require("path");
 
@@ -22,29 +21,4 @@ app.get("/survey", (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server draait op http://localhost:${PORT}`);
-=======
-const express = require("express");
-const path = require("path");
-
-const app = express();
-const PORT = 3000;
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.render("home", { title: "Carpool Platform" });
-});
-
-app.get("/survey", (req, res) => {
-    res.render("survey", { title: "Mobiliteitsbevraging" });
-});
-
-app.listen(PORT, () => {
-    console.log(`Server draait op http://localhost:${PORT}`);
->>>>>>> 94dcabec00df3774e864306abd5f636690a71c86
 });
